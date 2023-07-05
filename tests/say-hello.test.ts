606429a140052d25d24d4691ac9a12a016696ea8
akufarish <risfarishk@gmail.com>
2023-07-05 20:11:27 +0800

    belajar typescript dasar

--- tests/say-hello.test.ts
+++ tests/say-hello.test.ts
@@ -5,4 +5,8 @@ describe("sayHello", function () {
   it("should return hello Farish", function () {
     expect(sayHello("Farish")).toBe("Hello Farish");
   });
+
+  it("should return hello Farish Asqalani", function () {
+    expect(sayHello("Farish Asqalani")).toBe("Hello Farish Asqalani");
+  });
 });
