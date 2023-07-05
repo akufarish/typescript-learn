1ee17d9f4dc75bf3107fb9842addb76faf795c2a
akufarish <risfarishk@gmail.com>
2023-07-05 18:27:54 +0800

    belajar typescript dasar

--- tests/say-hello.test.ts
+++ tests/say-hello.test.ts
@@ -1,5 +1,6 @@
 import { sayHello } from "../src/say-hello";
 
+// test
 describe("sayHello", function () {
   it("should return hello Farish", function () {
     expect(sayHello("Farish")).toBe("Hello Farish");
